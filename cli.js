38ea051d50c6266167a78e9eed8422eab1f7e8ae
greffnoah <greffnoah@gmail.com>
2022-05-15 23:56:04 +0200

    added cli.js

---
+++ cli.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const { ArgumentParser } = require('argparse')
+const { createGenerator } = require('./index')
+
+const parser = new ArgumentParser({
+	description: 'Chunky Region Generator CLI'
+})
+
+parser.add_argument('--what', '-w', {
+	help: `Specify what to generate.`,
+	choices: ['metadata', 'image'],
+	required: true
+})
+parser.add_argument('--configs', '-c', {
+	help: `Specify the path the generator's config file.`,
+	required: true
+})
+parser.add_argument('--index', '-i', {
+	help: `Specify which region to focus on. (non zero-based)`
+})
+parser.add_argument('--all', '-a', {
+	help: `Specify whether to generate all regions.`
+})
+
+const main = async args => {
+	const configs = require(args.configs)
+	const generator = createGenerator(configs)
+	const method = args.what === 'image' ? 'generateImage' : 'generateMetadata'
+
+	if (args.all) {
+		for (let i = 1; i <= configs.totalCount; i++) {
+			await generator[method](i)
+		}
+	} else {
+		await generator[method](args.index)
+	}
+}
+
+main(parser.parse_args())
